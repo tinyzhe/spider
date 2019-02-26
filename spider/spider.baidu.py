f411008a93216b5bd7f920e71f98ea857b133fe3
tinyzhe <1161538461@qq.com>
2019-02-26 10:54:46 +0800

    新增翻页功能，默认抓取前十页内容

--- spider/spider.baidu.py
+++ spider/spider.baidu.py
@@ -17,13 +17,14 @@ import sys
 import re
 from conf import config
 from libs import dbmanager
+from helpers import log
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 class Processor():
     #百度搜索url
-    url = "https://baidu.com/s?wd="
+    url = "https://baidu.com"
     #模拟浏览器请求头
     requestHeaders = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -35,6 +36,8 @@ class Processor():
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36",
     }
 
+    exceptionAreaIds = []
+
     def __init__(self):
         self.__db = dbmanager.DB(conf=config.DB)
 
@@ -42,33 +45,32 @@ class Processor():
         self.spider()
 
     def spider(self):
-        areas = self.__db.selectAll('god_area',columns="cid,slug,category_name",where="status = 1 order by cid asc,level asc")
+        areas = self.__db.selectAll('god_area',columns="cid,slug,category_name",where="status = 1 and level < 3 order by cid asc,level asc")
         for area in areas:
             searchKey = str(area['category_name']) + '林长制'
-            url  = self.url + searchKey
+            url  = self.url + "/s?wd=" + searchKey
             resCode = requests.get(url, headers=self.requestHeaders, timeout=20)
             resCode.encoding = 'utf-8'
             bsCode = BeautifulSoup(resCode.text, "lxml")
-            content = bsCode.select('#content_left')
-            list = bsCode.find_all("div",class_="result")
-            for info in list:
-                title = str(info.find("a").get_text())
-                href = info.find("a").get("href")
-                abstract = str(info.find("div",class_="c-abstract").get_text())
-                realUrl = self.get_real(href)
-                keywords = ['会议','评审','试行','考察']
-                for key in keywords:
-                    if title.find(key) != -1 or abstract.find(key) != -1:
-                        sourceInfo = self.__db.selectOne('god_baidu_spider_page',where="resource_url = '%s'" % realUrl)
-                        if not sourceInfo:
-                            params = {"title":title,"abstract":abstract,"search_key":searchKey,"area_id":area['cid'],"baidu_url":href,"resource_url":realUrl}
-                            self.__db.insert('god_baidu_spider_page',params_dic=params)
-                            print title
-                            print realUrl
-                            print abstract
-                            print '---分割线---'
-                            break
-            #time.sleep(3)
+
+            self.formatInfo(bsCode=bsCode,area=area,searchKey=searchKey)
+
+            pageDiv = bsCode.find("div",id="page")
+            pageList = pageDiv.find_all("a")
+            pageInfo = {}
+            for p in pageList:
+                pageUrl = p.get("href")
+                pageUrl = self.url + str(pageUrl)
+                page = p.get_text()
+                if page != '下一页':
+                    pageInfo[page] = pageUrl
+                    print '--page is %s--' % page
+                    print '--pageUrl is %s--' % pageUrl
+                    pageResCode = requests.get(pageUrl, headers=self.requestHeaders, timeout=20)
+                    pageResCode.encoding = 'utf-8'
+                    pageBsCode = BeautifulSoup(resCode.text, "lxml")
+                    self.formatInfo(bsCode=pageBsCode, area=area, searchKey=searchKey)
+            time.sleep(10)
 
 
     def get_real(self, o_url):
@@ -81,6 +83,30 @@ class Processor():
                 pass
         return o_url  # 返回源地址
 
+    def formatInfo(self,bsCode,area,searchKey):
+        content = bsCode.select('#content_left')
+        list = bsCode.find_all("div", class_="result")
+        for info in list:
+            title = str(info.find("a").get_text())
+            href = info.find("a").get("href")
+            abstract = str(info.find("div", class_="c-abstract").get_text())
+            try:
+                realUrl = self.get_real(href)
+            except Exception as e:
+                log.logger.info(e)
+                self.exceptionAreaIds.append(area['cid'])
+                log.logger.info(','.join(self.exceptionAreaIds))
+
+            keywords = ['会议', '评审', '试行', '考察', '启动', '推行', '规划','改革']
+            for key in keywords:
+                if title.find(key) != -1 or abstract.find(key) != -1:
+                    sourceInfo = self.__db.selectOne('god_baidu_spider_page', where="resource_url = '%s'" % realUrl)
+                    if not sourceInfo:
+                        params = {"title": title, "abstract": abstract, "search_key": searchKey, "area_id": area['cid'],
+                                  "resource_url": realUrl}
+                        self.__db.insert('god_baidu_spider_page', params_dic=params)
+                        print title
+                        break
 
 
 Processor().run()
